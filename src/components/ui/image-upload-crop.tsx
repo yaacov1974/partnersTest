f0a03305fc93588cac7db7861c4f245e7c8d930e
biznes <yaacov@biznes.co.il>
2025-12-21 15:03:37 +0200

    Description of what I changed

--- src/components/ui/image-upload-crop.tsx
+++ src/components/ui/image-upload-crop.tsx
@@ -22,6 +22,7 @@ export function ImageUploadWithCrop({
   aspectRatio = 5 / 1,
   initialImage,
   className,
+  dropzoneClassName,
   circularCrop = false,
 }: ImageUploadWithCropProps) {
   const [selectedFile, setSelectedFile] = useState<string | null>(null);
